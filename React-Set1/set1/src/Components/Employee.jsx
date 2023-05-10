2d29a11e50f821ff3874a6e45f2523fd1596b68c
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-05-10 16:42:48 +0530

    EmployeeCard

---
+++ React-Set1/set1/src/Components/Employee.jsx
@@ -0,0 +1,34 @@
+/**Build an EmployeeCard component in React to display name, designation and work experience of a person. Pass name, designation and work experience as props.
+
+The colour of “Designation:” should be green.
+The colour of “Experience:” should be blue. 
+ */
+
+const EmployeeCard = 
+[{ name:"Ronaldo", designation:"Football", workExperience:"8 years" },
+{ name:"Dhoni", designation:"Cricket", workExperience:"6 years" },
+{ name:"Sania Mirza", designation:"Tennis", workExperience:"11 years" },
+{ name:"Saina Nehwal", designation:"Badminton", workExperience:"12 years" }
+]
+ 
+
+export const Employee  =() => {
+
+    return(
+        <div>
+            <h1>Employee </h1>
+            {
+                EmployeeCard.map(employee => {
+                                    return(
+                                        <div key={employee.name}>
+                                            <h2>Name: {employee.name}</h2>
+                                            <h3 style={{color:"green"}}>Designation: {employee.designation}</h3>
+                                            <h3 style={{color:"blue"}}>Work Experience{employee.workExperience}</h3>
+                                        </div>
+                                    )
+                                })
+            }
+        </div>
+    )
+    
+}
